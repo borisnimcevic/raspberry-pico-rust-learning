1bf37fa6fbcc9ae61ba93c80c89c45f22252364b
Boris Nimcevic <borisnimcevic@gmail.com>
2022-09-02 11:10:34 +0200

    Minimum to flash the rp pico.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use rp_pico::entry;
+
+// Ensure we halt the program on panic (if we don't mention this crate it won't
+// be linked)
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    // some setup
+
+    loop {
+        // toggle LED (GPIO 25)
+    }
 }
